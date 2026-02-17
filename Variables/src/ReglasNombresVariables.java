71855f13f32a97c9fb8771ad641476640a57bbec
PedroSL0904 <pedrolucio.0904@gmail.com>
2026-02-16 21:49:47 -0600

    Clase de variables

--- Variables/src/ReglasNombresVariables.java
+++ Variables/src/ReglasNombresVariables.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class ReglasNombresVariables {
+    public static void main(String[] args) {
+        // Reglas nombres variables
+        String nombreCompleto = "Pedro Sánchez Lucio"; // Correcto y buenas practicas
+        System.out.println("nombreCompleto = " + nombreCompleto);
+        String NombreCompleto = "Pedro Sánchez Lucio"; // Correcto, no aplica las buenas practicas
+        System.out.println("NombreCompleto = " + NombreCompleto);
+        // String nombre-cliente = "Pedro"; // Incorrecto
+        String nombre_cliente = "Pedro"; // Correcto, no aplica buenas practicas
+        String _apellido = "Sánchez"; // Correcto y aceptable
+        String $apellidoCompleto = "Sánchez Lucio"; // Correcto y aceptable
+        int totpzs = 10; // Correcto, no aplica buenas practicas
+        int totalPiezas = 10; // Correcto, aplica las buenas practicas
+        boolean casado = true; // Correcto, aun puede mejorar
+        boolean esCasado = true; // Correcto, aplica las buenas practicas
+        boolean isCasado = true; // Correcto, aplica las buenas practicas
+        boolean tieneSaldo = true; // Correcto, aplica las buenas practicas
+        boolean hasSaldo = true; // Correcto, aplica las buenas practicas
+    }
 }
--- Variables/src/TiposDatos.java
+++ Variables/src/TiposDatos.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
-
 public class TiposDatos {
+    public static void main(String[] args) {
+        // Tipos de datos en Java
+        //Enteros (su valor por default es 0)
+        byte tipoByte = 127;
+        System.out.println("tipoByte = " + tipoByte);
+        short tipoShort = 32000;
+        System.out.println("tipoShort = " + tipoShort);
+        int tipoEntero = 21473847;
+        System.out.println("tipoEntero = " + tipoEntero);
+        long tipoLong = 987654321098765432L;
+        System.out.println("tipoLong = " + tipoLong);
+
+        // Punto flotante (su valor por default es 0.0)
+        float tipoFloat = 3.14F; // F o f para indicar tipo float
+        System.out.println("tipoFloat = " + tipoFloat);
+        double tipoDouble = 3.1315D; // D o d para indicar tipo double (opcional)
+        System.out.println("tipoDouble = " + tipoDouble);
+
+        // Caracter (su valor por default es '\u0000')
+        char tipoChar = 'A'; // Caracteres del tipo unicode
+        System.out.println("tipoChar = " + tipoChar);
+        tipoChar = 65;
+        System.out.println("tipoChar = " + tipoChar);
+        tipoChar = '@';
+        System.out.println("tipoChar = " + tipoChar);
+
+        // Booleano (su valor por default es false)
+        boolean tipoBoolean = true;
+        System.out.println("tipoBoolean = " + tipoBoolean);
+        tipoBoolean = false;
+        System.out.println("tipoBoolean = " + tipoBoolean);
+
+        // Object (su valor por default es null)
+        String nombre = null;
+        System.out.println("nombre = " + nombre);
+        nombre = "Pedro Sanchez";
+        System.out.println("nombre = " + nombre);
+    }
 }
